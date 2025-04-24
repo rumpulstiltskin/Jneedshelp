630eaaed4546ff0869a4bd791273649c208c7cf8
Syed Husain <sahusain1984@gmail.com>
2025-04-24 07:06:42 -0400

    Add files via upload

---
+++ search-feature.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const { searchChatHistory } = require('./chat-controller');
+
+app.post('/search', (req, res) => {
+  const { searchTerm } = req.body;
+  searchChatHistory(searchTerm).then(searchResults => {
+    res.json(searchResults);
+  }).catch(err => res.status(500).send('An error occurred'));
+});
+
+app.get('/display', (req, res) => {
+  const message = req.query.message;
+  res.send(`
+${message}
+`);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
